bafd12012073b777754a33b856b72b6089449ec1
Nicole Dow <nikki.dow71@gmail.com>
2019-07-16 16:39:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -24,6 +24,14 @@ def key_for_min_value(hash)
   if hash.empty?
     return nil
   else
-    min_value = 
+    min_value = Float::INFINITY
+    min_key = ""
+    hash.each do |key, val|
+      if val < min_value
+        min_value = val
+        min_key = key
+      end
+    end
   end
+  min_key
 end
