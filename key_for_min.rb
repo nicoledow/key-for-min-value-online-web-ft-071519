0c973d3eebedcbac62db4738649fb8c21682f53a
Nicole Dow <nikki.dow71@gmail.com>
2019-07-16 16:22:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  minimum = 
+  minimum = hash.first
   
   name_hash.each do |name, info|
     binding.pry
